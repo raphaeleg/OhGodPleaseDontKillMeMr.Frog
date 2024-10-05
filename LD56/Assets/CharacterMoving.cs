ad63199c69c7339944745ac3c3df3d46c9b87125
Sei <94898980+SeiyoungJ@users.noreply.github.com>
2024-10-06 02:38:05 +0800

    Character Moving + Structure of day

--- LD56/Assets/CharacterMoving.cs
+++ LD56/Assets/CharacterMoving.cs
@@ -10,8 +10,19 @@ public class CharacterMoving : MonoBehaviour
     {
         //transform.DOMove(new Vector3(1250, 560, 0), cyclelength).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);//
         transform.DOMoveX(1300, 5);
-        Lily.DORotate(new Vector3(0, 0, 10), cyclelength/3).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+        Lily.DORotate(new Vector3(0, 0, 7), cyclelength * 0.35f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
 
+        //Asking for an animal [Event Dialogue / popup]
+
+        //Wait until the the player does an action that provides them with the animal
+
+        //Give Money based on if they liked it / Adjust Suspicion meter
+
+        //Leave
+
+        //Repeat 2 more times
+
+        //After this go to Night Time
 
     }
 }
