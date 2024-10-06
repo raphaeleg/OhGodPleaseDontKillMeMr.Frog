6db0f1a4ebadf104b9c438ee8382b6d4d33114a9
Samkitt Patni <samkitt.patni@gmail.com>
2024-10-06 13:43:06 +0100

    Updated Night gameplay to switch to minigames

--- LD56/Assets/Scripts/NightManager.cs
+++ LD56/Assets/Scripts/NightManager.cs
@@ -9,10 +9,35 @@ public class NightManager : MonoBehaviour
     [SerializeField] private Inventory inventory;
     [SerializeField] private Animator exoticAnimalAnim;
     [SerializeField] private TMP_Text exoticAnimalText;
+    [SerializeField] private SceneLoader loader;
+
+    private string[] minigames = { "StartMinigameLockpick", "StartMinigameFishing", "StartMinigameDino" };
 
     public void OnEnable()
     {
         exoticAnimalAnim.Play(inventory.requestAnimal.animal.GetSpeciesName());
         exoticAnimalText.text = "Reward: $" + inventory.requestAnimal.money;
+    }
+
+    public void StealAction()
+    {
+        int day = inventory.day;
+        Debug.Log(day);
+        if (day == 1)
+        {
+            EventManager.TriggerEvent(minigames[0]);
+        }
+        else if (day == 2)
+        {
+            EventManager.TriggerEvent(minigames[1]);
+        }
+        else if (day == 3)
+        {
+            EventManager.TriggerEvent(minigames[2]);
+        }
+        else
+        {
+            EventManager.TriggerEvent(minigames[Random.Range(0, minigames.Length-1)]);
+        }
     }
 }
--- LD56/Assets/Scripts/SceneLoader.cs
+++ LD56/Assets/Scripts/SceneLoader.cs
@@ -1,15 +1,49 @@
 using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Collections;
+using System;
 
 public class SceneLoader : MonoBehaviour
 {
     private static Animator sceneAnimator;
     private static string nextLevel;
 
+    #region EventManager
+    private Dictionary<string, Action<int>> SubscribedEvents;
+
+    private void OnEnable()
+    {
+        StartCoroutine("DelayedSubscription");
+    }
+    private IEnumerator DelayedSubscription()
+    {
+        yield return new WaitForSeconds(0.0001f);
+        foreach (var pair in SubscribedEvents)
+        {
+            EventManager.StartListening(pair.Key, pair.Value);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (SubscribedEvents == null) { return; }
+        foreach (var pair in SubscribedEvents)
+        {
+            EventManager.StopListening(pair.Key, pair.Value);
+        }
+    }
+    #endregion
+
     private void Awake()
     {
         sceneAnimator = GetComponent<Animator>();
+        SubscribedEvents = new() {
+            { "StartMinigameLockpick", LoadMinigameLockpick },
+            { "StartMinigameFishing", LoadMinigameFishing },
+            { "StartMinigameDino", LoadMinigameDino },
+        };
     }
     public void LoadScene()
     {
@@ -34,12 +68,29 @@ public class SceneLoader : MonoBehaviour
 
     public static void LoadGameplayDay()
     {
+        EventManager.TriggerEvent("NextDay");
         FadeLevel("Gameplay_Day");
     }
 
     public static void LoadGameplayNight()
     {
         FadeLevel("Gameplay_Night");
+    }
+
+    public static void LoadMinigameLockpick(int index = 0)
+    {
+        Debug.Log("GameCalled");
+        FadeLevel("Minigame_Lockpick");
+    }
+
+    public static void LoadMinigameFishing(int index = 0)
+    {
+        FadeLevel("Minigame_Fishing");
+    }
+
+    public static void LoadMinigameDino(int index = 0)
+    {
+        // TODO: Wait for game implementation
     }
 
     public static void LoadSettings()
