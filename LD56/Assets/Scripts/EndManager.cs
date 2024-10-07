59d0c5c55db415b095cd9ddb8102f67db3c6ecad
Samkitt Patni <samkitt.patni@gmail.com>
2024-10-07 10:54:42 +0100

    Updated Ending UI

--- LD56/Assets/Scripts/EndManager.cs
+++ LD56/Assets/Scripts/EndManager.cs
@@ -7,28 +7,22 @@ public class EndManager : MonoBehaviour
 {
     [SerializeField] private Inventory inventory;
     [SerializeField] private TMP_Text gameEndText;
-    //private static GameManager Instance;
+    [SerializeField] private GameObject background;
 
     // Start is called before the first frame update
     void Awake()
     {
         if (GameManager.Instance.getSuspicion() >= 100)
         {
-            gameEndText.text = "Police captured you!";
+            gameEndText.text = "Oh No! You were too Suspicious!! The Police captured you! (Ending 1/3)";
         }
         else if (GameManager.Instance.getMoney() < 400)
         {
-            gameEndText.text = "Mafia killed you!";
+            gameEndText.text = "Oh No! You didn't collect enough Money!! The Mafia was unhappy and killed you! (Ending 2/3)";
         }
         else
         {
-            gameEndText.text = "You get to live another day!";
+            gameEndText.text = "Horray!! You get to live another day!! (Ending 3/3)";
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
