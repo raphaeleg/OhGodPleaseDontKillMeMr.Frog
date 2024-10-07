ba99350586d127a34e8e9dd00c7b172154c61949
raphaeleg <raphaeleguillemotmichelle@gmail.com>
2024-10-08 02:07:06 +0800

    Tweaks to Day-Night Animation

--- LD56/Assets/Scripts/Anim_Night_Envelope.cs
+++ LD56/Assets/Scripts/Anim_Night_Envelope.cs
@@ -19,20 +19,32 @@ public class Anim_Night_Envelope : MonoBehaviour
 
     private IEnumerator EnvelopeAnimation()
     {
+        float waiting = 2.75f;
+        
+        //yield return new WaitForSeconds(0.5f);
+
+        dayShift.SetActive(true);
+        dayShift.GetComponent<Animator>().Play("DayToNight");
+
+        yield return new WaitForSeconds(waiting);
+
+        //dayShift.GetComponent<Animator>().StopPlayback();
+        dayShift.GetComponent<CanvasGroup>().DOFade(0, 1);
+        //yield return new WaitForSeconds(2f);
+        //dayShift.SetActive(false);
+
+
+
+
         gameObject.transform.GetChild(0).GetComponent<Animator>().Play("Envelope");
         gameObject.transform.GetChild(0).transform.localScale = Vector3.zero;
         gameObject.transform.GetChild(0).transform.DOScale(Vector3.one, 2).SetEase(Ease.InOutSine);
         yield return new WaitForSeconds(2f);
+
         gameObject.transform.GetChild(0).GetComponent<CanvasGroup>().DOFade(0, 1);
+
         yield return new WaitForSeconds(1f);
+
         transform.GetComponent<CanvasGroup>().DOFade(0, 1);
-        //yield return new WaitForSeconds(0.5f);
-        dayShift.SetActive(true);
-        dayShift.GetComponent<Animator>().Play("DayToNight");
-        yield return new WaitForSeconds(2f);
-        //dayShift.GetComponent<Animator>().StopPlayback();
-        dayShift.GetComponent<CanvasGroup>().DOFade(0, 2);
-        yield return new WaitForSeconds(2f);
-        dayShift.SetActive(false);
     }
 }
